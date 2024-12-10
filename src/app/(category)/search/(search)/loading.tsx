849b167d42d48d4dbbe4df7656b8502ec6b9d3a1
sumittttpaul <sumitpaul.work@gmail.com>
2024-12-10 16:14:29 +0530

    v24.1.H2

--- src/app/(category)/search/(search)/loading.tsx
+++ src/app/(category)/search/(search)/loading.tsx
@@ -1,63 +1,16 @@
-"use client";
-
-import Skeleton from "@mui/material/Skeleton";
-import useMediaQuery from "@mui/material/useMediaQuery";
-
 export default function Loading() {
-  const NotMobileScreen = useMediaQuery("(min-width:768px)");
   return (
-    <div className="mx-auto flex w-full max-w-[70rem] flex-col space-y-5 px-2.5 pt-5 md:mt-2.5 md:space-y-7 md:px-5 md:pt-10">
-      <Skeleton
-        height={NotMobileScreen ? 75 : 60}
-        variant="rectangular"
-        className="w-[150px] rounded-xl md:w-[200px] md:rounded-2xl"
-      />
+    <div className="mx-auto flex w-full max-w-[70rem] animate-pulse flex-col space-y-5 px-2.5 pt-5 md:mt-2.5 md:px-5 md:pt-10">
+      <span className="h-[55px] w-[150px] rounded-xl bg-gray-200 md:h-[75px] md:w-[200px] md:rounded-2xl" />
       <div className="flex w-full flex-col space-y-2.5 md:space-y-5">
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
-        <Skeleton
-          height={334}
-          variant="rectangular"
-          className="w-full rounded-xl md:rounded-2xl"
-        />
+        {[...Array(5)].map((o, i) => {
+          return (
+            <span
+              key={i}
+              className="h-[334px] w-full rounded-xl bg-gray-200 md:rounded-2xl"
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,73 +1,34 @@
-"use client";
-
-import Skeleton from "@mui/material/Skeleton";
-import useMediaQuery from "@mui/material/useMediaQuery";
-
 export default function Loading() {
-  const NotMobileScreen = useMediaQuery("(min-width:768px)");
   return (
-    <div className="mx-auto flex w-full max-w-[110rem] flex-col space-y-2.5 px-2.5 pt-5 md:space-y-7 md:px-5 md:pt-10">
+    <div className="mx-auto flex w-full max-w-[110rem] animate-pulse flex-col space-y-2.5 px-2.5 pt-5 md:space-y-7 md:px-5 md:pt-10">
       <div className="flex w-full justify-between">
-        <Skeleton
-          height={NotMobileScreen ? 75 : 55}
-          variant="rectangular"
-          className="w-[125px] rounded-lg md:w-[200px] md:rounded-2xl"
-        />
-        <Skeleton
-          height={NotMobileScreen ? 75 : 60}
-          variant="rectangular"
-          className="w-[125px] rounded-lg md:w-[200px] md:rounded-2xl"
-        />
+        {[...Array(2)].map((o, i) => {
+          return (
+            <span
+              key={i}
+              className="h-[55px] w-[125px] rounded-lg md:h-[75px] md:w-[200px] md:rounded-2xl bg-gray-200"
+            />
+          );
+        })}
       </div>
       <div className="flex w-full flex-col">
-        <Skeleton
-          height={NotMobileScreen ? 880 : 288}
-          variant="rectangular"
-          className="w-full rounded-lg md:rounded-2xl"
-        />
+        <span className="h-[288px] w-full rounded-lg md:h-[880px] md:rounded-2xl bg-gray-200" />
       </div>
-      <div className="flex w-full justify-between pb-1 pt-1 md:pb-0 md:pt-5">
-        <Skeleton
-          height={NotMobileScreen ? 75 : 55}
-          variant="rectangular"
-          className="w-[125px] rounded-lg md:w-[200px] md:rounded-2xl"
-        />
-      </div>
-      <div className="flex w-full flex-col">
-        <Skeleton
-          height={NotMobileScreen ? 880 : 520}
-          variant="rectangular"
-          className="w-full rounded-lg md:rounded-2xl"
-        />
-      </div>
-      <div className="flex w-full justify-between pb-1 pt-1 md:pb-0 md:pt-5">
-        <Skeleton
-          height={NotMobileScreen ? 75 : 55}
-          variant="rectangular"
-          className="w-[125px] rounded-lg md:w-[200px] md:rounded-2xl"
-        />
-      </div>
-      <div className="flex w-full flex-col">
-        <Skeleton
-          height={NotMobileScreen ? 880 : 520}
-          variant="rectangular"
-          className="w-full rounded-lg md:rounded-2xl"
-        />
-      </div>
-      <div className="flex w-full justify-between pb-1 pt-1 md:pb-0 md:pt-5">
-        <Skeleton
-          height={NotMobileScreen ? 75 : 55}
-          variant="rectangular"
-          className="w-[125px] rounded-lg md:w-[200px] md:rounded-2xl"
-        />
-      </div>
-      <div className="flex w-full flex-col">
-        <Skeleton
-          height={NotMobileScreen ? 880 : 520}
-          variant="rectangular"
-          className="w-full rounded-lg md:rounded-2xl"
-        />
-      </div>
+      {[...Array(3)].map((o, i) => {
+        return (
+          <>
+            <div
+              key={i}
+              className="flex w-full justify-between pb-1 pt-1 md:pb-0 md:pt-5"
+            >
+              <span className="h-[55px] w-[125px] rounded-lg md:h-[75px] md:w-[200px] md:rounded-2xl bg-gray-200" />
+            </div>
+            <div key={i + 3} className="flex w-full flex-col">
+              <span className="h-[520px] w-full rounded-lg md:h-[880px] md:rounded-2xl bg-gray-200" />
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
